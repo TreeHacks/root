f398f95b90346c9b16382739d00fde9cdcf05f10
Ashwin Ramaswami <arama@Ashwins-MacBook-Pro.local>
2018-09-16 10:32:00 -0400

    Define mongoose schema for application info

--- src/models/Application.d.ts
+++ src/models/Application.d.ts
@@ -4,7 +4,21 @@ export interface IReview {
     
 }
 export interface IApplicationInfo {
-    university: Boolean
+    first_name: String,
+    last_name: String,
+    phone: String,
+    dob: String,
+    gender: String,
+    race: [String],
+    university: String,
+    graduation_year: String,
+    level_of_study: String,
+    major: String,
+    resume: String,
+    accept_terms: boolean,
+    accept_share: boolean,
+    q1_goodfit: String,
+    q2_experience: String
 }
 export interface IAdditionalInfo {
     bus_confirmed_spot: Boolean
@@ -18,17 +32,17 @@ export interface IApplication extends Document {
   },
   "admin_info": { // Only editable by admin.
       "transportation": {
-          "method": {type: string},
-          "bus_name": {type: string}
+          "method": {type: String},
+          "bus_name": {type: String}
       },
-      "reimbursement_amount": {type: string}
+      "reimbursement_amount": {type: String}
   },
   "reviews": [IReview// each review can only be modified by the reviewer who made it.
   ],
   "user": {
-    "email": {type: string}
+    "email": {type: String}
    }, // foreign key
-  "id": {type: string},
-  "status": {type: string}, // only editable by admin (or this user, to a limited extent). incomplete (default), submitted, admitted, waitlisted, rejected, admission_confirmed, admission_declined
-  "type": {type: string}
+  "id": {type: String},
+  "status": {type: String}, // only editable by admin (or this user, to a limited extent). incomplete (default), submitted, admitted, waitlisted, rejected, admission_confirmed, admission_declined
+  "type": {type: String}
 }
--- src/models/applicationInfoSchema.ts
+++ src/models/applicationInfoSchema.ts
@@ -1,7 +1,21 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 const applicationInfoSchema: Schema = new mongoose.Schema({
-  university: String
+  first_name: String,
+  last_name: String,
+  phone: String,
+  dob: String,
+  gender: String,
+  race: [String],
+  university: String,
+  graduation_year: String,
+  level_of_study: String,
+  major: String,
+  resume: String,
+  accept_terms: Boolean,
+  accept_share: Boolean,
+  q1_goodfit: String,
+  q2_experience: String
 }, { _id : false });
 
 export default applicationInfoSchema;
