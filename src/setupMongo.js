fd6efef4e83d296824e6cf5b2ed039e4d8d2d592
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-10-25 20:11:08 -0700

    Fix mongo setup

--- src/setupMongo.js
+++ src/setupMongo.js
@@ -9,7 +9,7 @@ const mongoServer = new MongodbMemoryServer.MongoMemoryServer({
 console.warn("Setting up mongodb server...");
 module.exports = async function () {
   await mongoServer.getConnectionString().then((uri) => {
-    process.env.MONGODB_CONN_STR = uri;
+    process.env.MONGO_CONN_STR = uri;
     console.log("Mongodb server running on url", uri);
 
     // Set reference to mongod in order to close the server during teardown.
