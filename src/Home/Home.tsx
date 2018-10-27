47e6cff5bc624055bc66354d5adce3551f14a104
Theo Patt <theo@theopatt.com>
2018-10-26 18:26:41 -0700

    nav - style fixes

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -30,7 +30,7 @@ class Login extends React.Component<IHomeProps, {}> {
         </NavLink>  
       </div>
       <div>
-        <NavLink to="/">dashboard</NavLink>
+        <NavLink to="/" isActive={(_, loc) => loc.pathname === "/"}>dashboard</NavLink>
         <NavLink to="/application_info">application</NavLink>
         <NavLink to="/additional_info">travel</NavLink>
         {this.props.auth.admin &&
