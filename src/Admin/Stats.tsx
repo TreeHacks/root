a97ab90ede6a56161b6e7f5e08c023df1d4e44f8
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-11-17 09:29:42 -0800

    Don't show null values on stats

--- src/Admin/Stats.tsx
+++ src/Admin/Stats.tsx
@@ -5,6 +5,7 @@ import Loading from "../Loading/Loading";
 import { getApplicationStats } from "../store/admin/actions";
 import { Pie, PieChart, Tooltip } from "recharts";
 import { IAdminState } from "../store/admin/types";
+import {filter} from "lodash-es";
 
 const Stats = (props: IStatsProps) => {
     return <div>
@@ -12,7 +13,7 @@ const Stats = (props: IStatsProps) => {
             {Object.keys(props.applicationStats).map(key => (
                 <div key={key} className="col-6 col-sm-4 col-md-4 col-lg-4 col-xl-3"><h3 className="text-white text-center">{key}</h3>
                     <PieChart width={250} height={250}>
-                        <Pie data={props.applicationStats[key]} label={true} dataKey="count" nameKey="_id" />
+                        <Pie data={filter(props.applicationStats[key], e => e._id !== null)} label={true} dataKey="count" nameKey="_id" />
                         <Tooltip />
                     </PieChart>
                 </div>
