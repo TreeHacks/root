097f6ce9f65fbf79542580e02ca458b7a1379518
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-11-16 16:51:43 -0800

    Add tooltips for stats

--- src/Admin/Stats.tsx
+++ src/Admin/Stats.tsx
@@ -3,16 +3,17 @@ import { connect } from "react-redux";
 import { IStatsWrapperProps, IStatsProps } from "./types";
 import Loading from "../Loading/Loading";
 import { getApplicationStats } from "../store/admin/actions";
-import { Pie, PieChart } from "recharts";
+import { Pie, PieChart, Tooltip } from "recharts";
 import { IAdminState } from "../store/admin/types";
 
 const Stats = (props: IStatsProps) => {
     return <div>
         <div className="row">
             {Object.keys(props.applicationStats).map(key => (
-                <div key={key} className="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2"><h3 className="text-white text-center">{key}</h3>
+                <div key={key} className="col-6 col-sm-4 col-md-4 col-lg-4 col-xl-3"><h3 className="text-white text-center">{key}</h3>
                     <PieChart width={250} height={250}>
                         <Pie data={props.applicationStats[key]} label={true} dataKey="count" nameKey="_id" />
+                        <Tooltip />
                     </PieChart>
                 </div>
             ))}
