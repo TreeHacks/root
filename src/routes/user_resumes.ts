f2ffdaada40fa9be375c888c5dbccc86c4ddc509
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-12-30 20:36:05 -0500

    Don't error when resume is not uploaded

--- src/routes/user_resumes.ts
+++ src/routes/user_resumes.ts
@@ -1,10 +1,9 @@
 import Application from "../models/Application";
 import { Request, Response } from 'express';
-import async from "async";
 import { getFile } from "../services/file_actions";
 import { S3 } from "aws-sdk";
 import JSZip from "jszip";
-import fs from "fs";
+import {get} from "lodash";
 
 export function getUserResumes(req: Request, res: Response) {
     // Todo: sponsor authentication; should not be able to get resumes of arbitrary ids.
@@ -12,12 +11,17 @@ export function getUserResumes(req: Request, res: Response) {
     .then(results => {
         let requests: Promise<S3.Types.GetObjectOutput>[] = [];
         for (let result of results) {
-            requests.push(getFile(result.forms.application_info.resume));
+            let resume = get(result, "forms.application_info.resume");
+            if (resume) {
+                requests.push(getFile(resume));
+            }
         }
         return Promise.all(requests).then(resumes => {
             const zip = JSZip();
             for (let i in resumes) {
-                zip.file(`${results[i].forms.application_info.first_name} ${results[i].forms.application_info.last_name}.pdf`, resumes[i].Body);
+                if (resumes[i] && resumes[i].Body) {
+                    zip.file(`${results[i].forms.application_info.first_name} ${results[i].forms.application_info.last_name}.pdf`, resumes[i].Body);
+                }
             }
             zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
             .pipe(res)
