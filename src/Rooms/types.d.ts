aa4605039b136cf8225df0e2e6ca478f8731d6d9
Theo Patt <theo@theopatt.com>
2019-02-07 22:28:55 -0800

    rooms: update types

--- src/Rooms/types.d.ts
+++ src/Rooms/types.d.ts
@@ -5,6 +5,6 @@ export interface IRoomsProps extends IFormState {
 
 export interface IRoomsState {
     now: number,
-    rooms: { id: string, name: string, expiry: Date, error: string }[],
+    rooms: { id: string, name: string, expiry: Date, error: string, next_unavailable?: { start: string, end: string, label: string} }[],
     currentRoom: any
 }
