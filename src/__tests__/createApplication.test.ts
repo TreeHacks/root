ba0b116b9a7ebd1941563792e45ae912c79510fb
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-09 16:59:27 -0800

    test: fix create application tests

--- src/__tests__/createApplication.test.ts
+++ src/__tests__/createApplication.test.ts
@@ -1,35 +1,45 @@
+import request from "supertest";
+import app from "../index";
+import Application from "../models/Application";
 import { createApplication } from "../routes/common";
 import { IApplication } from "../models/Application.d";
+import lolex from "lolex";
+import { STATUS } from "../constants";
 
-describe('test', () => {
-  
-    test('test', () => {
-    });
+beforeAll(async () => {
+    await request(app);
 });
 
-// describe('create application', () => {
+afterEach(() => {
+    return Application.deleteMany({});
+})
 
-//     test('should create stanford user application', async () => {
-//         expect.assertions(1);
-//         const application: IApplication = await createApplication({ email: "test@stanford.edu", sub: "123123" });
-//         expect(application.type).toEqual("stanford");
-//     });
 
-//     test('should create stanford user application with in-state', async () => {
-//         // Should never be called; stanford users should use SAML for SSO.
-//         const application: IApplication = await createApplication({ email: "test@stanford.edu", sub: "123124", "custom:location": "California" });
-//         expect(application.type).toEqual("stanford");
-//     });
+describe('create application before deadline', () => {
+    let clock;
+    beforeEach(() => {
+        clock = lolex.install({ now: new Date("01/01/1999") });
+    })
+    afterEach(() => {
+        clock.uninstall();
+    })
 
-//     test('should create user application in-state', async () => {
-//         const application: IApplication = await createApplication({ email: "test@berkeley.edu", sub: "123125", "custom:location": "California" });
-//         expect(application.type).toEqual("is");
+    test('should create stanford user application', async () => {
+        const application: IApplication = await createApplication({ email: "test@stanford.edu", sub: "123123" });
+        expect(application.type).toEqual("stanford");
+        expect(application.status).toEqual(STATUS.INCOMPLETE);
+    });
 
-//     });
+    test('should create user application in-state', async () => {
+        const application: IApplication = await createApplication({ email: "test@berkeley.edu", sub: "123125", "custom:location": "California" });
+        expect(application.type).toEqual("is");
+        expect(application.status).toEqual(STATUS.INCOMPLETE);
+    });
 
-//     test('should create user application out-of-state', async () => {
-//         const application: IApplication = await createApplication({ email: "test@gatech.edu", sub: "123126", "custom:location": "Georgia" });
-//         expect(application.type).toEqual("oos");
-//     });
+    test('should create user application out-of-state', async () => {
+        const application: IApplication = await createApplication({ email: "test@gatech.edu", sub: "123126", "custom:location": "Georgia" });
+        expect(application.type).toEqual("oos");
+        expect(application.status).toEqual(STATUS.INCOMPLETE);
+    });
 
-// });
+});
