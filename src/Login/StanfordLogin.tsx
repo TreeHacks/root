9709d4ef0804d3b819ffa06f9158ae91ba185a5f
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-13 19:42:23 -0800

    fix: remove state from saml auth

--- src/Login/StanfordLogin.tsx
+++ src/Login/StanfordLogin.tsx
@@ -8,12 +8,11 @@ interface StanfordLoginProps {
 
 export default (props: StanfordLoginProps) => (
     <form action={`${COGNITO_ENDPOINT_URL}/oauth2/authorize`} method="GET" className="login-btn-style">
-        <input type="hidden" name="identity_provider" value="Stanford" />
         <input type="hidden" name="response_type" value="code" />
         <input type="hidden" name="client_id" value={COGNITO_CLIENT_ID} />
-        <input type="hidden" name="scope" value="aws.cognito.signin.user.admin email openid phone profile" />
         <input type="hidden" name="redirect_uri" value={window.location.origin} />
-        <input type="hidden" name="state" value="123" />
+        <input type="hidden" name="scope" value="aws.cognito.signin.user.admin email openid phone profile" />
+        <input type="hidden" name="identity_provider" value="Stanford" />
         <input type="submit" className="btn btn-stanford" value={props.label || 'Sign in with Stanford'} />
     </form>
 );
