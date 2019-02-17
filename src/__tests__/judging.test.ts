c11d50ecf21871ff77cd48d6b3d1319833a08e26
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-17 11:16:33 -0800

    fix: fix typo in tests

--- src/__tests__/judging.test.ts
+++ src/__tests__/judging.test.ts
@@ -488,7 +488,7 @@ describe('judge stats', () => {
             .set({ Authorization: 'judge' })
             .expect(200)
             .then(e => {
-                expect(e.body.results.num_reamining).toEqual(2);
+                expect(e.body.results.num_remaining).toEqual(2);
             })
     });
 });
