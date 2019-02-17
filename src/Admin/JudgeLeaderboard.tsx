647246eae106d8a280ead7eade4a4dc356d722ee
Theo Patt <theo@theopatt.com>
2019-02-17 10:47:06 -0800

    + refresh on judging stats

--- src/Admin/JudgeLeaderboard.tsx
+++ src/Admin/JudgeLeaderboard.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { API } from "aws-amplify";
 
-export default class JudgeLeaderboard extends React.Component<{}, { leaderboard_data: any, stats_data: any }> {
+export default class JudgeLeaderboard extends React.Component<{}, { refreshing: boolean, leaderboard_data: any, stats_data: any }> {
     constructor(props) {
         super(props);
         this.state = {
+            refreshing: false,
             leaderboard_data: null,
             stats_data: null
         }
@@ -13,19 +14,23 @@ export default class JudgeLeaderboard extends React.Component<{}, { leaderboard_
         this.loadData();
     }
     async loadData() {
+        this.setState({ refreshing: true });
+
         Promise.all([
             API.get("treehacks", '/judging/leaderboard', {}),
             API.get("treehacks", '/judging/stats', {})
         ]).then(([leaderboard_data, stats_data]) => {
             window.scrollTo(0, 0);
-            this.setState({ leaderboard_data, stats_data });
+            this.setState({ leaderboard_data, stats_data, refreshing: false });
         }).catch((err) => {
             alert("Error, " + err);
+            this.setState({ refreshing: false });
             console.log(err);
         });
     }
     render() {
         return (<div className="container">
+            <div style={{marginTop: 10}}><a href="#" onClick={e => { this.loadData(); e.preventDefault(); }}>{this.state.refreshing ? "Refreshing..." : "Refresh"}</a></div>
             <table className="table">
                 <tbody>
                     {this.state.leaderboard_data && this.state.leaderboard_data.map(person => <tr key={person._id}>
