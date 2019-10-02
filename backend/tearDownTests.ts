03c931863170d9d1214b688311681363c0922f9a
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-10-02 01:18:45 +0000

    increase async timeout

--- backend/tearDownTests.ts
+++ backend/tearDownTests.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import Application from "./models/Application";
 
+jest.setTimeout(30000);
+
 module.exports = async () => {
     await Application.deleteMany({});
     await mongoose.connection.close();
