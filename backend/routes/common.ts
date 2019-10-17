8b3d3030cd351438ef7b2e15b60e59007863e1ce
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-10-17 00:30:10 +0000

    update deadlines again

--- backend/routes/common.ts
+++ backend/routes/common.ts
@@ -11,12 +11,12 @@ import { Model } from "mongoose";
 export function getDeadline(type) {
   switch (type) {
       case "is":
-          return new Date("2019-11-27T07:59:00.000Z");
+          return new Date("2019-11-26T07:59:00.000Z");
       case "stanford":
-          return new Date("2020-02-18T07:59:00.000Z");
+          return new Date("2020-02-14T07:59:00.000Z");
       case "oos":
       default:
-          return new Date("2019-11-20T07:59:00.000Z");
+          return new Date("2019-11-19T07:59:00.000Z");
   }
 }
 
--- src/themes/custom.ts
+++ src/themes/custom.ts
@@ -4,20 +4,20 @@ export default {
         {
             "key": "oos",
             "label": "out-of-state",
-            "date": "2019-11-20T07:59:00.000Z",
-            "display_date": "November 19, 2019"
+            "date": "2019-11-19T07:59:00.000Z",
+            "display_date": "November 18, 2019 11:59 PM PT"
         },
         {
             "key": "is",
             "label": "in-state",
-            "date": "2019-11-27T07:59:00.000Z",
-            "display_date": "November 26, 2019"
+            "date": "2019-11-26T07:59:00.000Z",
+            "display_date": "November 25, 2019 11:59 PM PT"
         },
         {
             "key": "stanford",
             "label": "Stanford student",
-            "date": "2020-02-18T07:59:00.000Z",
-            "display_date": "as soon as possible"
+            "date": "2020-02-14T07:59:00.000Z",
+            "display_date": "February 13, 2020 11:59 PM PT"
         }
     ],
     "logo": require('./assets/logo.svg'),
