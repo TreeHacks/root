c10b2c43671fe29317f0e4b90f1beb5eacab24a3
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-11-08 01:06:32 +0000

    refactor to DateChart component

--- src/Admin/Stats.tsx
+++ src/Admin/Stats.tsx
@@ -7,36 +7,33 @@ import { Pie, PieChart, Tooltip, LineChart, XAxis, YAxis, CartesianGrid, Line, R
 import { IAdminState } from "../store/admin/types";
 import { filter } from "lodash";
 
+const DateChart = ({ data, children }) => <LineChart width={800} height={500}
+    data={data}>
+    <XAxis dataKey="date" tickFormatter={timeStr => new Date(timeStr).toLocaleDateString()} />
+    <YAxis />
+    <Legend />
+    <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+    {children}
+    <Tooltip />
+</LineChart>;
+
 const Stats = (props: IStatsProps) => {
     return <div>
         <div className="row">
             {Object.keys(props.applicationStats).map(key => (
                 key === "timeline" ?
                     <div key={key} className="col-12"><h3 className="text-center">{key}</h3>
-                        <LineChart width={800} height={500}
-                            data={props.applicationStats[key]}>
-                            {/* data={[{num_is: 0, num_oos: 0, num_stanford: 10, date: "2019-10-25T15:46:22.000Z"}, {num_is: 50, num_oos: 0, num_stanford: 10, date: "2019-10-26T15:46:22.000Z"}, {num_is: 52, num_oos: 0, num_stanford: 10, date: "2019-10-27T15:46:22.000Z"}]}> */}
-                            <XAxis dataKey="date" tickFormatter={timeStr => new Date(timeStr).toLocaleDateString()} />
-                            <YAxis />
-                            <Legend />
-                            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                        <DateChart data={props.applicationStats[key]}>
                             <Line type="monotone" dataKey="num_is" stroke="red" />
                             <Line type="monotone" dataKey="num_oos" stroke="green" />
                             <Line type="monotone" dataKey="num_stanford" stroke="blue" />
                             <Line type="monotone" dataKey="num_total" stroke="black" />
-                            <Tooltip />
-                        </LineChart><br />
-                        <LineChart width={800} height={500}
-                            data={props.applicationStats[key]}>
-                            <XAxis dataKey="date" tickFormatter={timeStr => new Date(timeStr).toLocaleDateString()} />
-                            <YAxis />
-                            <Legend />
-                            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                        </DateChart>
+                        <DateChart data={props.applicationStats[key]}>
                             <Line type="monotone" dataKey="num_submitted" stroke="red" />
                             <Line type="monotone" dataKey="num_incomplete" stroke="green" />
                             <Line type="monotone" dataKey="num_total" stroke="black" />
-                            <Tooltip />
-                        </LineChart>
+                        </DateChart>
                     </div>
                     :
                     <div key={key} className="col-6 col-sm-4 col-md-4 col-lg-4 col-xl-3"><h3 className="text-center">{key}</h3>
