cadb5d3554e699b49fd27f74cda764dd17b4600c
Lucas Pauker <lucas.pauker@gmail.com>
2020-01-10 15:39:02 -0800

    Name themes, change default to black and white instead of last year's theme

---
+++ src/themes/old_themes/green_computer.ts
@@ -0,0 +1,28 @@
+export default {
+    "hackathon_year": 2019,
+    "hackathon_date_range": "February 15-17",
+    "locations": ["Outside USA", "Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", "US Territories"],
+    "deadlines": [
+        {
+            "key": "oos",
+            "label": "out-of-state",
+            "date": "2018-11-20T07:59:00.000Z",
+            "display_date": "November 19, 2018"
+        },
+        {
+            "key": "is",
+            "label": "in-state",
+            "date": "2018-11-27T07:59:00.000Z",
+            "display_date": "November 26, 2018"
+        },
+        {
+            "key": "stanford",
+            "label": "Stanford student",
+            "date": "2019-02-18T07:59:00.000Z",
+            "display_date": "as soon as possible"
+        }
+    ],
+    "logo": require('../art/logo.svg'),
+    "favicon": require("../art/favicon.png"),
+    "dashboard_background": require("../art/combined_circuit.svg")
+}
