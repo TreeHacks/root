5efdd209e546b3c71a707dbed88d213eff371f9b
Lucas Pauker <lucas.pauker@gmail.com>
2020-02-06 19:35:11 -0800

    Update script to not hardcode database name

--- backend/scripts/fetch_slack_profile_pictures.py
+++ backend/scripts/fetch_slack_profile_pictures.py
@@ -1,29 +1,33 @@
 import os
 import slack
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo import MongoClient, uri_parser
 
 dotenv_path = '.env'
 load_dotenv(dotenv_path)
-client = MongoClient(os.environ['MONGODB_URI'], retryWrites=False);
-db = client['heroku_2r7kmznv']
-applications = db.applications.find()
+
+mongo_uri = os.environ['MONGODB_URI']
+client = MongoClient(mongo_uri, retryWrites=False);
+uri_dict = uri_parser.parse_uri(mongo_uri)
+db = client[uri_dict['database']]
+applications = db.applications.find({'year': '2020', 'forms.meet_info': {'$exists': True}})
 slack_client = slack.WebClient(token=os.environ['SLACK_OAUTH_ACCESS_TOKEN'])
 
 for app in applications:
-    if 'meet_info' in app['forms']:
-        user_email = app['user']['email']
-        try:
-            user_resp = slack_client.users_lookupByEmail(email=user_email)
-        except slack.errors.SlackApiError:
-            continue
-        image_link = user_resp['user']['profile']['image_512']
-        db.applications.update_one({
-            '_id': app['_id']
-        },{
-            '$set': {
-                'forms.meet_info.profilePicture': image_link
-            }
-        }, upsert=False)
+    user_email = app['user']['email']
+    try:
+        user_resp = slack_client.users_lookupByEmail(email=user_email)
+    except slack.errors.SlackApiError:
+        continue
+    image_link = user_resp['user']['profile']['image_512']
+    if image_link == app['forms']['meet_info']['profilePicture']:
+        continue
+    db.applications.update_one({
+        '_id': app['_id']
+    },{
+        '$set': {
+            'forms.meet_info.profilePicture': image_link
+        }
+    }, upsert=False)
 
 client.close()
