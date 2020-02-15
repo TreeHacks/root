5e4f941012b881b03f41fce788b6e745889e5314
Lucas Pauker <lucas.pauker@gmail.com>
2020-02-14 22:27:01 -0800

    Update leaderboard route

--- backend/routes/leaderboard.ts
+++ backend/routes/leaderboard.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import axios from "axios";
+import Application from "../models/Application";
+const mongoose = require('mongoose')
 
 export async function leaderboard(req: Request, res: Response) {
     const url = "https://api.eventive.org/reports/passholder_tickets_tags";
@@ -9,9 +11,73 @@ export async function leaderboard(req: Request, res: Response) {
     }
     try {
         const response = await axios.post(url, data);
+        let ids = [];
+        let display_types = [];
+        for (var key in response.data) {
+          var user_id = response.data[key]["data"]["data_1548654864366"]
+          var event_data = {};
+          if (user_id) {
+            event_data = response.data[key]["attendance"]
+          }
+          user_id && ids.push({
+            id: user_id,
+            events: event_data
+          });
+
+          Object.keys(event_data).map(key =>
+            !display_types.includes(key) && display_types.push(key)
+          );
+        }
+
+        let sorted_data = []
+        display_types.map(type => {
+          let type_data = [];
+          ids.map(id => {
+            if (Object.keys(id["events"]).includes(type)) {
+              type_data.push({
+                id: id["id"],
+                num: id["events"][type]
+              })
+            }
+          });
+          type_data = type_data.sort(
+            function (a,b) {
+              return b["num"] - a["num"];
+            }
+          ).slice(0, 10);
+          sorted_data.push({
+            type: type,
+            data: type_data
+          });
+        });
+
+        let best_ids = [];
+        sorted_data.map(type_data =>
+          type_data["data"].map(data_point => {
+            !best_ids.includes(data_point["id"]) && best_ids.push(data_point["id"]);
+          })
+        );
+        best_ids = ["e221eed3-4bcb-4ff3-91fc-30ca734118d0"];
+        const user_response = await Application.find({"user.id": {$in: best_ids}}, {"forms.application_info.first_name": 1, "forms.application_info.last_name": 1, "forms.meet_info.profilePicture": 1, "user.id": 1});
+
+        user_response.map(data =>
+          sorted_data.map(type_data =>
+            type_data["data"].map(data_point => {
+              console.log(data.user["id"]);
+              console.log(data_point["id"]);
+              if (data.user["id"] == data_point["id"]) {
+                console.log("lajdlkaj");
+                data_point["first_name"] = data.forms.application_info.first_name;
+                data_point["last_name"] = data.forms.application_info.last_name;
+                data_point["picture"] = data.forms.meet_info.profilePicture;
+              }
+            })
+          )
+        );
+
         return res.json({
             success: true,
-            data: response.data
+            data: sorted_data
         });
     } catch (e) {
         res.status(500).json({
