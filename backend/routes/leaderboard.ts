a9da4474df86e8e97f9dcec79c20e70e0611813d
Lucas Pauker <lucas.pauker@gmail.com>
2020-02-14 23:57:08 -0800

    key update

--- backend/routes/leaderboard.ts
+++ backend/routes/leaderboard.ts
@@ -3,6 +3,15 @@ import axios from "axios";
 import Application from "../models/Application";
 const mongoose = require('mongoose')
 
+function getKey(obj) {
+  for (let k of Object.keys(obj)) {
+    if (obj[k].indexOf("-") > -1) {
+      return k;
+    }
+  }
+  return null;
+}
+
 export async function leaderboard(req: Request, res: Response) {
     const url = "https://api.eventive.org/reports/passholder_tickets_tags";
     const data = {
@@ -14,7 +23,7 @@ export async function leaderboard(req: Request, res: Response) {
         let ids = [];
         let display_types = [];
         for (var key in response.data) {
-          var user_id = response.data[key]["data"]["data_1548654864366"]
+          var user_id = response.data[key]["data"][getKey(response.data[key]["data"])];
           var event_data = {};
           if (user_id) {
             event_data = response.data[key]["attendance"]
