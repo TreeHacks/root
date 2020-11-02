bcfad024e2f338dd7d23dacca6d7c5363b09d85f
Katz <mykatz01@gmail.com>
2020-11-01 17:46:50 -0800

    Dates workaround, 2020 -> 2021

--- src/themes/green_sprout.ts
+++ src/themes/green_sprout.ts
@@ -9,6 +9,12 @@ export default {
           "display_date": "December 15, 2020"
       },
       {
+        "key": "is",
+        "label": "Non-Stanford student",
+        "date": "2020-12-16T07:59:00.000Z",
+        "display_date": "December 15, 2020"
+      },
+      {
           "key": "stanford",
           "label": "Stanford student",
           "date": "2021-01-15T07:59:00.000Z",
