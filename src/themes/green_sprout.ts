59d1d58cab31a177d465752abdb33e0e9373d76a
Katz <mykatz01@gmail.com>
2020-12-15 14:00:17 -0800

    dec22

--- src/themes/green_sprout.ts
+++ src/themes/green_sprout.ts
@@ -1,27 +1,27 @@
 export default {
-  "hackathon_year": 2021,
-  "hackathon_date_range": "February 12-14",
-  "deadlines": [
-      {
-          "key": "oos",
-          "label": "Non-Stanford student",
-          "date": "2020-12-23T07:59:00.000Z",
-          "display_date": "December 23, 2020"
-      },
-      {
-          "key": "is",
-          "label": "Non-Stanford student",
-          "date": "2020-12-23T07:59:00.000Z",
-          "display_date": "December 23, 2020"
-      },
-      {
-          "key": "stanford",
-          "label": "Stanford student",
-          "date": "2021-01-15T07:59:00.000Z",
-          "display_date": "January 15, 2021"
-      }
+  hackathon_year: 2021,
+  hackathon_date_range: "February 12-14",
+  deadlines: [
+    {
+      key: "oos",
+      label: "Non-Stanford student",
+      date: "2020-12-23T07:59:00.000Z",
+      display_date: "December 22, 2020",
+    },
+    {
+      key: "is",
+      label: "Non-Stanford student",
+      date: "2020-12-23T07:59:00.000Z",
+      display_date: "December 22, 2020",
+    },
+    {
+      key: "stanford",
+      label: "Stanford student",
+      date: "2021-01-15T07:59:00.000Z",
+      display_date: "January 15, 2021",
+    },
   ],
-  "logo": require('./assets/logo.svg'),
-  "favicon": require("./assets/favicon.ico"),
-  "dashboard_background": require("./assets/combined_circuit.svg")
+  logo: require("./assets/logo.svg"),
+  favicon: require("./assets/favicon.ico"),
+  dashboard_background: require("./assets/combined_circuit.svg"),
 };
