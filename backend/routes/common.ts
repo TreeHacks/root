6422888fbb37ad3844aab29160d4c1122097ce40
Marco Pizarro <marcoinnovates@gmail.com>
2021-12-26 02:11:31 -0800

    Change Due Time

--- backend/routes/common.ts
+++ backend/routes/common.ts
@@ -12,12 +12,12 @@ import { prepopulateMeetInfo } from "./meet_info";
 export function getDeadline(type) {
     switch (type) {
         case "is":
-            return new Date("2021-12-26T07:59:00.000Z");
+            return new Date("2021-12-27T07:59:00.000Z");
         case "stanford":
             return new Date("2022-01-30T07:59:00.000Z");
         case "oos":
         default:
-            return new Date("2021-12-26T07:59:00.000Z");
+            return new Date("2021-12-27T07:59:00.000Z");
     }
 }
 
