7cf90a1d339524cfe54f559393fffe685ffc5ce0
Marco Pizarro <marcoinnovates@gmail.com>
2021-12-26 02:07:45 -0800

    DateChange

--- src/themes/bonsai_chip.ts
+++ src/themes/bonsai_chip.ts
@@ -1,19 +1,19 @@
 export default {
     // TODO: Make these dates the actual dates of the hackathon
-    "hackathon_year": 2022,
+    "hackathon_year": 2021,
     "hackathon_date_range": "February 18-20",
     "locations": ["In-Person", "Virtually"],
     "deadlines": [
         {
             "key": "oos",
             "label": "out-of-state",
-            "date": "2021-12-27T16:00:00.000Z",
+            "date": "2021-12-27T07:59:00.000Z",
             "display_date": "December 26, 2021"
         },
         {
             "key": "is",
             "label": "in-state",
-            "date": "2021-12-27T16:00:00.000Z",
+            "date": "2021-12-27T07:59:00.000Z",
             "display_date": "December 26, 2021"
         },
         {
