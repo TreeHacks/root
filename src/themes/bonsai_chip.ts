23db41445c19f89aaa2e0b36ebb1f40f769ca2b4
Marco Pizarro <marcoinnovates@gmail.com>
2021-12-27 00:02:04 -0800

    Extend Deadline

--- src/themes/bonsai_chip.ts
+++ src/themes/bonsai_chip.ts
@@ -7,14 +7,14 @@ export default {
         {
             "key": "oos",
             "label": "out-of-state",
-            "date": "2021-12-27T07:59:00.000Z",
-            "display_date": "December 26, 2021"
+            "date": "2022-01-03T07:59:00.000Z",
+            "display_date": "January 2, 2022"
         },
         {
             "key": "is",
             "label": "in-state",
-            "date": "2021-12-27T07:59:00.000Z",
-            "display_date": "December 26, 2021"
+            "date": "2022-01-03T07:59:00.000Z",
+            "display_date": "January 2, 2022"
         },
         {
             "key": "stanford",
