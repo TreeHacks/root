6377cd8f5e5e30ee0fda26d52248de929e7eb5c6
sathviknallamalli <sathviknallamalli@gmail.com>
2022-11-29 17:10:02 -0800

    initial

---
+++ src/themes/timber_pine.ts
@@ -0,0 +1,29 @@
+export default {
+  // TODO: Make these dates the actual dates of the hackathon
+  hackathon_year: 2023,
+  hackathon_date_range: "February 17-19",
+  locations: ["In-Person", "Virtually"],
+  deadlines: [
+    {
+      key: "oos",
+      label: "out-of-state",
+      date: "2023-01-03T07:59:00.000Z",
+      display_date: "January 2, 2023",
+    },
+    {
+      key: "is",
+      label: "in-state",
+      date: "2023-01-03T07:59:00.000Z",
+      display_date: "January 2, 2023",
+    },
+    {
+      key: "stanford",
+      label: "Stanford student",
+      date: "2023-02-18T07:59:00.000Z",
+      display_date: "Feb 17, 2023",
+    },
+  ],
+  logo: require("./assets/logo.svg"),
+  favicon: require("./assets/favicon.ico"),
+  dashboard_background: require("./assets/combined_circuit.svg"),
+};
