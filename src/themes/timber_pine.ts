d9c832b7fa2affe481ade8e3baddc03af35cd958
sathviknallamalli <sathviknallamalli@gmail.com>
2023-01-03 00:05:53 -0800

    extension

--- src/themes/timber_pine.ts
+++ src/themes/timber_pine.ts
@@ -63,13 +63,13 @@ export default {
     {
       key: "oos",
       label: "out-of-state",
-      date: "2023-01-03T07:59:00.000Z",
+      date: "2023-01-03T08:45:00.000Z",
       display_date: "January 2, 2023",
     },
     {
       key: "is",
       label: "in-state",
-      date: "2023-01-03T07:59:00.000Z",
+      date: "2023-01-03T08:45:00.000Z",
       display_date: "January 2, 2023",
     },
     {
