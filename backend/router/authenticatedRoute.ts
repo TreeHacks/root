009f6d6bf7f4bfac945fdb5a7bfe2e33a04c02c5
GashonHussein <gashon96@gmail.com>
2023-02-18 16:13:41 -0800

    fix: fix middleware

--- backend/router/authenticatedRoute.ts
+++ backend/router/authenticatedRoute.ts
@@ -38,13 +38,12 @@ authenticatedRoute.use(function(req, res, next) {
       if (application && application.status === "admission_confirmed") {
         next();
       } else if (application) {
-        //sponsor may have accidentally went to root.treehacks, which will create an application
-        //so if someone does have an application, and is not confirmed, but is a sponsor, let them in
-        let middleware = validateGroup(["sponsor"], true);
-        middleware(req, res, next);
-
-        middleware = validateGroup(["mentor"], true);
-        middleware(req, res, next);
+        const sponsor = await SponsorAdmin.findOne({ email: response.email });
+        if (sponsor) {
+          return next();
+        }
+        const middleware = validateGroup("mentor");
+        return middleware(req, res, next);
       } else if (!application) {
         const sponsor = await SponsorAdmin.findOne({ email: response.email });
         if (sponsor) {
