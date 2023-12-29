8ae725234427901ca832a3443b82555ef475088d
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-12-28 18:41:24 -0800

    Update used_meals.ts

--- backend/routes/used_meals.ts
+++ backend/routes/used_meals.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express';
 import { getApplicationAttribute, setApplicationAttribute } from "./common";
 import { IApplication } from '../models/Application.d';
 
-export function getMeetInfo(req: Request, res: Response) {
+export function getUsedMeals(req: Request, res: Response) {
     return getApplicationAttribute(req, res, (e: IApplication) => {
-        return e.used_meals || {};
+        return e.forms.used_meals || {};
     }, true);
 }
 
-export function setMeetInfo(req: Request, res: Response) {
+export function setUsedMeals(req: Request, res: Response) {
     return setApplicationAttribute(req, res,
         (e: IApplication) => {
             e.forms.used_meals = req.body;
