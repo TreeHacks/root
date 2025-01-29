151927c8e9f3511ac40df8a88679dfc3b31cec4a
Connor Fogarty <connorflynnfogarty@gmail.com>
2025-01-29 02:44:05 -0800

    fix: update judge schema + interface

--- backend/models/Judge.ts
+++ backend/models/Judge.ts
@@ -3,16 +3,46 @@ import { Model, Schema } from "mongoose";
 
 interface IJudge extends mongoose.Document {
     _id: string,
-    email: string,
-    verticals: string[],
-    floor: number
+    year: string,
+    forms: {
+        application_info: {
+            first_name: string,
+            last_name: string
+        },
+        check_in_info: {
+            tshirtSize: string
+        },
+        meal_info: {
+            usedMeals: string[],
+            dietaryRestrictions: string
+        }
+    },
+    user: {
+        id: string,
+        email: string
+    }
 }
 
 const judgeSchema: Schema = new mongoose.Schema({
     "_id": String,
-    "email": String,
-    "verticals": [String],
-    "floor": Number
+    "year": String,
+    "forms": {
+        "application_info": {
+            "first_name": String,
+            "last_name": String
+        },
+        "check_in_info": {
+            "tshirtSize": String
+        },
+        "meal_info": {
+            "usedMeals": [String],
+            "dietaryRestrictions": String
+        }
+    },
+    "user": {
+        "id": String,
+        "email": String
+    }
 });
 
 const model: Model<IJudge> = mongoose.model("Judge", judgeSchema);
