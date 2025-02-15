674b390b791d5b89929f4750ca6d0f0075970210
Connor Fogarty <connorflynnfogarty@gmail.com>
2025-02-14 23:57:07 -0800

    Remove sponsor application table + link to meet

--- src/Sponsors/Sponsors.tsx
+++ src/Sponsors/Sponsors.tsx
@@ -4,16 +4,19 @@ import { RoutedTabs, NavTab } from 'react-router-tabs';
  
 import 'react-router-tabs/styles/react-router-tabs.scss';
 import SponsorsTable from './SponsorsTable';
+import { HACKATHON_YEAR } from '../constants';
  
 const Sponsors = ({ match }) => {
   return (
     <div className="bg-white col-8 offset-2 p-4">
-      <NavTab to={`${match.path}/table`}>Application Table</NavTab>
+      Welcome to TreeHacks {HACKATHON_YEAR}! We're so excited to have you as a sponsor this year. Please visit <a href="https://meet.treehacks.com">our sponsor Meet portal</a> to learn about this year's hackers.
+
+      {/* <NavTab to={`${match.path}/table`}>Application Table</NavTab>
        
       <Switch>
         <Route exact path={`${match.path}`} render={() => <Redirect replace to={`${match.path}/table`} />} />
         <Route path={`${match.path}/table`} component={SponsorsTable} />
-      </Switch>
+      </Switch> */}
     </div>
   );
 };
